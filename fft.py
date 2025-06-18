80a7fa1012ac3e4bee9d1b289a6ee3cf5c51c9c1
Celeste Roselli <72556836+celesteroselli@users.noreply.github.com>
2025-06-18 12:52:45 -0400

    FFT working for sine waves

--- fft.py
+++ fft.py
@@ -1,31 +1,42 @@
+import matplotlib.pyplot as plt
 import numpy as np
-global N
+#MUST BE A FACTOR OF TWO
+intervals = 128
 
-#must have length as factor of 2
-x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+xvals = np.linspace(0, 2*np.pi, intervals)
+samples = np.sin(50*xvals)+np.sin(10*xvals)
+
 #should go to 2
-N = len(x)
+frequencies = []
 
-global final
-final = 0
+#max frequency is N/2, so test all frequencies until N/2
 
 print("\n \n \n \n \n")
 
-def sum(until, k, a, c, p):
-    z = 2*p
-    print("N/" + str(z) + " = " + str(until))
-    if (until == 1):
-        index = a
-        print(index)
-        return x[index] * np.exp((-1j * np.pi * k))
+distance = xvals[1]-xvals[0]
+
+#x is terms
+def split(x):
+    N = len(x)
+    if (N==1):
+        #exponent will be 1 bc e^0 = 1
+        return x
     else:
-        #even
-        print("creating even branch")
-        even = sum(until/2, k, a, 2*c, p+1)
+        x_even = split(x[::2]) #all even-ordered elements (step of 2 starting at 0)
+        x_odd = split(x[1::2]) #all odd-ordered elements (step of 2 starting at 1)
 
-        #odd - difference is a vs a + p
-        print("creating odd branch")
-        odd = sum(until/2, k, a+p, 2*c, p+1)
-        return even + odd
+        #for each frequency, 
+        m_list = [0] * N
+        for k in range(N//2):
+            exp = np.exp((-2j * np.pi * k)/N)
+            m_list[k] = (x_even[k] + exp*x_odd[k])
+            m_list[k + N//2] = x_even[k] - exp*x_odd[k]
+        return m_list
 
-final = sum(N/2, 3, 0, 1, 1)
+freq_axis = [0] * (intervals)
+freq_axis = [k for k in range(intervals)]
+
+frequencylist = split(samples)
+
+plt.plot(freq_axis[1:intervals//2], (np.abs(frequencylist)[1:intervals//2]))
+plt.show()
